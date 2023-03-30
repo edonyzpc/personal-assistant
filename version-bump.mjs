d8a26c0aaa0f6a2acfd518d9c00c2156b0f870c7
edonyzpc <edonyzpc@yahoo.com>
2023-03-30 08:19:11 +0000

    v1.0.6

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,6 +7,10 @@ let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+// read minAppVersion from manifest.json and bump version to target version
+let manifestBeta = JSON.parse(readFileSync("manifest-beta.json", "utf8"));
+manifestBeta.version = targetVersion;
+writeFileSync("manifest-beta.json", JSON.stringify(manifestBeta, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
