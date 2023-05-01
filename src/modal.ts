dba71473ea6f303b61cbbed887b93e5e507a4ab7
edonyzpc <edonyzpc@yahoo.com>
2023-05-01 17:19:05 +0800

    feat: unify plugin switch on/off as one command
    - unify plugin switch on/off as one command
    - improve UI with adding plugin status
    
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- src/modal.ts
+++ src/modal.ts
@@ -1,4 +1,6 @@
-import { App, Notice, SuggestModal } from 'obsidian'
+import { App, Notice, SuggestModal, addIcon, setIcon } from 'obsidian'
+
+import { icons } from './utils';
 
 interface Plugin {
     name: string;
@@ -7,17 +9,15 @@ interface Plugin {
     enbaled: boolean;
 }
 
-export const OpenPlugin = true;
-export const ClosePlugin = false;
-
 export class PluginControlModal extends SuggestModal<Plugin> {
     private obsidianPlugins: any; // eslint-disable-line @typescript-eslint/no-explicit-any
     private toEnablePlugin: boolean;
+    private enabledColor = "green";
+    private disabledColor = "red";
 
-    constructor(app: App, toEnable: boolean) {
+    constructor(app: App) {
         super(app);
         this.obsidianPlugins = (app as any).plugins; // eslint-disable-line @typescript-eslint/no-explicit-any
-        this.toEnablePlugin = toEnable;
     }
 
     // Returns all available suggestions.
@@ -25,37 +25,48 @@ export class PluginControlModal extends SuggestModal<Plugin> {
         'use strict'
         const disabledPlugins: Plugin[] = [];
         const enabledPlugins: Plugin[] = [];
+        const plugins: Plugin[] = [];
         for (const key of Object.keys((window.app as any).plugins.manifests)) { // eslint-disable-line @typescript-eslint/no-explicit-any
+            const pluginObject: Plugin = {
+                name: this.obsidianPlugins.manifests[key].name,
+                id: this.obsidianPlugins.manifests[key].id,
+                desc: this.obsidianPlugins.manifests[key].description,
+                enbaled: false
+            };
             // find disabled plugins
             if (!this.obsidianPlugins.enabledPlugins.has(this.obsidianPlugins.manifests[key].id)) {
-                disabledPlugins.push({
-                    name: this.obsidianPlugins.manifests[key].name,
-                    id: this.obsidianPlugins.manifests[key].id,
-                    desc: this.obsidianPlugins.manifests[key].description,
-                    enbaled: false,
-                });
+                pluginObject.enbaled = false;
+                disabledPlugins.push(pluginObject);
             } else {
-                enabledPlugins.push({
-                    name: this.obsidianPlugins.manifests[key].name,
-                    id: this.obsidianPlugins.manifests[key].id,
-                    desc: this.obsidianPlugins.manifests[key].description,
-                    enbaled: true,
-                });
+                pluginObject.enbaled = true;
+                enabledPlugins.push(pluginObject);
             }
+            plugins.push(pluginObject);
         }
-        return this.toEnablePlugin ? disabledPlugins.filter((plugin) => plugin.name.toLowerCase().includes(query.toLowerCase())) : enabledPlugins.filter((plugin) => plugin.name.toLowerCase().includes(query.toLowerCase()));
+        //return this.toEnablePlugin ? disabledPlugins.filter((plugin) => plugin.name.toLowerCase().includes(query.toLowerCase())) : enabledPlugins.filter((plugin) => plugin.name.toLowerCase().includes(query.toLowerCase()));
+        return plugins.filter((plugin) => plugin.name.toLowerCase().includes(query.toLowerCase()));
     }
 
     // Renders each suggestion item.
     renderSuggestion(plugin: Plugin, el: HTMLElement) {
-        el.createEl("div", { text: plugin.name });
+        const color = plugin.enbaled ? this.enabledColor : this.disabledColor;
+
+        addIcon('SWITCH_ON_STATUS', icons['SWITCH_ON_STATUS']);
+        addIcon('SWITCH_OFF_STATUS', icons['SWITCH_OFF_STATUS']);
+        const div = el.createEl("div", { attr: { style: `color: ${color}` } });
+        if (plugin.enbaled) {
+            setIcon(div, 'SWITCH_ON_STATUS');
+        } else {
+            setIcon(div, 'SWITCH_OFF_STATUS');
+        }
+        div.querySelector('svg')?.addClass("plugin-swith-on-off-svg");
+        div.createSpan({ text: plugin.name, attr: { style: "color: var(--text-normal)" } });
         el.createEl("small", { text: plugin.desc });
     }
 
     // Perform action on the selected suggestion.
     onChooseSuggestion(plugin: Plugin, evt: MouseEvent | KeyboardEvent) {
-        'use strict'
-        if (this.toEnablePlugin) {
+        if (!plugin.enbaled) {
             if (this.obsidianPlugins.enablePluginAndSave(plugin.id)) {
                 new Notice(`enable plugin[${plugin.name}] successfully`);
             } else {
