93d8ae5c11b3544828b9b42032c68102b9d021fe
edonyzpc <edonyzpc@yahoo.com>
2023-06-16 16:51:34 +0800

    feat: list callouts command supporting quickly callout-insert
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- src/callout.ts
+++ src/callout.ts
@@ -1,201 +1,240 @@
-import { Component, MarkdownRenderer, Modal } from 'obsidian'
+import { App, Component, SuggestModal, getIcon } from 'obsidian'
+import type {  Callout, CalloutID } from 'obsidian-callout-manager';
 
-type Release = {
-	tag_name: string;
-	body: string;
-	draft: boolean;
-	prerelease: boolean;
-};
+import type { PluginManager } from './plugin';
+import { type RGB, parseColorRGB } from './color';
 
-async function getReleaseNotesAfter(
-	repoOwner: string,
-	repoName: string,
-	releaseTagName: string
-): Promise<Release[]> {
-	const response = await fetch(
-		`https://api.github.com/repos/${repoOwner}/${repoName}/releases`
-	);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const releases: Release[] | { message: string } = await response.json();
 
-	if ((!response.ok && "message" in releases) || !Array.isArray(releases)) {
-		throw new Error(
-			`Failed to fetch releases: ${releases.message ?? "Unknown error"}`
-		);
-	}
+export class CalloutModal extends SuggestModal<Callout> {
+	private plugin: PluginManager;
 
-	const startReleaseIdx = releases.findIndex(
-		(release) => release.tag_name === releaseTagName
-	);
+    constructor(app: App, plugin: PluginManager) {
+        super(app);
+		this.plugin = plugin;
+    }
 
-	if (startReleaseIdx === -1) {
-		throw new Error(`Could not find release with tag ${releaseTagName}`);
-	}
+    // Returns all available suggestions.
+    getSuggestions(query: string): Callout[] {
+        const callouts = this.plugin.calloutManager?.getCallouts();
+        if (callouts) {
+            return callouts.concat().filter((callout) => callout.id.toLowerCase().includes(query.toLowerCase()));
+        }
+        // default callout
+        return [{
+                "id": "quote",
+                "icon": "quote-glyph",
+                "color": "158, 158, 158",
+                "sources": [
+                    {
+                    "type": "builtin"
+                    },
+                    {
+                    "type": "snippet",
+                    "snippet": "callout"
+                    }
+                ]
+            }]
+    }
 
-	return releases
-		.slice(0, startReleaseIdx)
-		.filter((release) => !release.draft && !release.prerelease);
+    // Renders each suggestion item.
+    renderSuggestion(callout: Callout, el: HTMLElement) {
+        const calloutContainerEl = el.createEl('div');
+        calloutContainerEl.classList.add('calloutmanager-preview-container');
+        calloutContainerEl.setAttribute('data-callout-manager-callout', callout.id);
+        const { icon, id } = callout;
+        const color = getColorFromCallout(callout);
+        new CalloutPreviewComponent(calloutContainerEl, {
+            id,
+            icon,
+            title: getTitleFromCallout(callout),
+            color: color ?? undefined,
+        });
+    }
+
+    // Perform action on the selected suggestion.
+    async onChooseSuggestion(callout: Callout, evt: MouseEvent | KeyboardEvent) {
+        const title = getTitleFromCallout(callout);
+        const calloutMarkdownContent = `> [!${callout.id}] ${title}
+> Contents
+
+`
+        await navigator.clipboard.writeText(calloutMarkdownContent);
+    }
 }
 
-function addExtraHashToHeadings(
-	markdownText: string,
-	numHashes = 1
-): string {
-	// Split the markdown text into an array of lines
-	const lines = markdownText.split("\n");
+const NO_ATTACH = Symbol();
 
-	// Loop through each line and check if it starts with a heading syntax (#)
-	for (let i = 0; i < lines.length; i++) {
-		if (lines[i].startsWith("#")) {
-			// If the line starts with a heading syntax, add an extra '#' to the beginning
-			lines[i] = "#".repeat(numHashes) + lines[i];
+export interface PreviewOptions {
+	/**
+	 * The callout ID.
+	 */
+	id: CalloutID;
+
+	/**
+	 * The icon to display in the callout.
+	 * This should be known in advance.
+	 */
+	icon: string;
+
+	/**
+	 * The color of the callout.
+	 */
+	color?: RGB;
+
+	/**
+	 * The title to show.
+	 * The callout ID will be used if this is omitted.
+	 */
+	title?: HTMLElement | DocumentFragment | string | ((titleEl: HTMLElement) => unknown);
+
+	/**
+	 * The content to show.
+	 */
+	content?: HTMLElement | DocumentFragment | string | ((contentEl: HTMLElement) => unknown);
+}
+
+/**
+ * A component that displays a preview of a callout.
+ */
+export class CalloutPreviewComponent extends Component {
+	public readonly calloutEl: HTMLElement;
+	public readonly contentEl: HTMLElement | undefined;
+	public readonly titleEl: HTMLElement;
+	public readonly iconEl: HTMLElement;
+
+	public constructor(containerEl: HTMLElement | typeof NO_ATTACH, options: PreviewOptions) {
+		super();
+		const { color, icon, id, title, content } = options;
+
+		const frag = document.createDocumentFragment();
+
+		// Build the callout.
+		const calloutEl = (this.calloutEl = frag.createDiv({ cls: ['callout', 'calloutmanager-preview'] }));
+		const titleElContainer = calloutEl.createDiv({ cls: 'callout-title' });
+		this.iconEl = titleElContainer.createDiv({ cls: 'callout-icon' });
+		const titleEl = (this.titleEl = titleElContainer.createDiv({ cls: 'callout-title-inner' }));
+		const contentEl = (this.contentEl =
+			content === undefined ? undefined : calloutEl.createDiv({ cls: 'callout-content' }));
+
+		this.setIcon(icon);
+		this.setColor(color);
+		this.setCalloutID(id);
+
+		// Set the callout title.
+		if (title == null) titleEl.textContent = id;
+		else if (typeof title === 'function') title(titleEl);
+		else if (typeof title === 'string') titleEl.textContent = title;
+		else titleEl.appendChild(title);
+
+		// Set the callout contents.
+		if (contentEl != null) {
+			if (typeof content === 'function') content(contentEl);
+			else if (typeof content === 'string') contentEl.textContent = content;
+			else contentEl.appendChild(content as HTMLElement | DocumentFragment);
+		}
+
+		// Attach to the container.
+		if (containerEl != NO_ATTACH) {
+			CalloutPreviewComponent.prototype.attachTo.call(this, containerEl);
 		}
 	}
 
-	// Join the array of lines back into a single string and return it
-	return lines.join("\n");
-}
-
-export class CalloutModal extends Modal {
-	releases: Release[];
-	private releaseNotesPromise: Promise<Release[]>;
-	private previousVersion: string;
-
-	constructor(previousQAVersion: string) {
-		super(app);
-		this.previousVersion = previousQAVersion;
-
-		this.releaseNotesPromise = getReleaseNotesAfter(
-			"chhoumann",
-			"quickadd",
-			previousQAVersion
-		);
-		this.releaseNotesPromise
-			.then((releases) => {
-				this.releases = releases;
-
-                if (this.releases.length === 0) {
-                    this.close();
-                    return;
-                }
-
-				this.display();
-			})
-			.catch((err) => {
-				console.log(`Failed to fetch release notes: ${err as string}`);
-			});
+	/**
+	 * Changes the callout ID.
+	 * This will *not* change the appearance of the preview.
+	 *
+	 * @param id The new ID to use.
+	 */
+	public setCalloutID(id: string): typeof this {
+		const { calloutEl } = this;
+		calloutEl.setAttribute('data-callout', id);
+		return this;
 	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.empty();
-		contentEl.createEl("h1", {
-			text: "Fetching release notes...",
-		});
+	/**
+	 * Changes the callout icon.
+	 *
+	 * @param icon The ID of the new icon to use.
+	 */
+	public setIcon(icon: string): typeof this {
+		const { iconEl, calloutEl } = this;
+
+		// Change the icon style variable.
+		calloutEl.style.setProperty('--callout-icon', icon);
+
+		// Clear the icon element and append the SVG.
+		iconEl.empty();
+		const iconSvg = getIcon(icon);
+		if (iconSvg != null) {
+			this.iconEl.appendChild(iconSvg);
+		}
+
+		return this;
 	}
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
+	/**
+	 * Changes the callout color.
+	 *
+	 * @param color The color to use.
+	 */
+	public setColor(color: RGB | undefined): typeof this {
+		const { calloutEl } = this;
+
+		if (color == null) {
+			calloutEl.style.removeProperty('--callout-color');
+			return this;
+		}
+
+		calloutEl.style.setProperty('--callout-color', `${color.r}, ${color.g}, ${color.b}`);
+		return this;
 	}
 
-	private display(): void {
-		const { contentEl } = this;
-		contentEl.empty();
-		contentEl.classList.add("quickadd-update-modal-container");
+	/**
+	 * Attaches the callout preview to a DOM element.
+	 * This places it at the end of the element.
+	 *
+	 * @param containerEl The container to attach to.
+	 */
+	public attachTo(containerEl: HTMLElement): typeof this {
+		containerEl.appendChild(this.calloutEl);
+		return this;
+	}
 
-        const header = `### New in QuickAdd v${this.releases[0].tag_name}\n`
-		const text = `Thank you for using QuickAdd! If you like the plugin, please consider supporting me by buying me a coffee. With your sponsorship, I'll be able to contribute more to my existing projects, start new ones, and be more responsive to issues & feature requests.`;
-		const buymeacoffee = `<div class="quickadd-bmac-container"><a href="https://www.buymeacoffee.com/chhoumann" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 40px !important;width: 144px !important;" ></a></div>`;
-
-		const contentDiv = contentEl.createDiv("quickadd-update-modal");
-		const releaseNotes = this.releases
-			.map((release) => release.body)
-			.join("\n---\n");
-
-		const andNow = `And now, here is everything new in QuickAdd since your last update (v${this.previousVersion}):`;
-        const feedbackForm = `I'd love to get your feedback on QuickAdd! Please fill out this <a href="https://forms.gle/WRq1ewcKK8qmkqps6">feedback form</a> to let me know what you think.`;
-		const markdownStr = `${header}\n${text}\n${buymeacoffee}\n${feedbackForm}\n\n${andNow}\n\n---\n\n${addExtraHashToHeadings(
-			releaseNotes
-		)}`;
-
-		void MarkdownRenderer.renderMarkdown(
-			markdownStr,
-			contentDiv,
-			app.vault.getRoot().path,
-			new Component(),
-		);
+	/**
+	 * Resets the `--callout-color` and `--callout-icon` CSS properties added to the callout element.
+	 */
+	public resetStylePropertyOverrides() {
+		const { calloutEl } = this;
+		calloutEl.style.removeProperty('--callout-color');
+		calloutEl.style.removeProperty('--callout-icon');
 	}
 }
 
-export class UpdateModal extends Modal {
-	releases: Release[];
-	private releaseNotesPromise: Promise<Release[]>;
-	private previousVersion: string;
+/**
+ * Gets the title of a callout.
+ *
+ * This should be the same as what Obsidian displays when a callout block does not have a user-specified title.
+ *
+ * @param callout The callout.
+ * @returns The callout's title.
+ */
+export function getTitleFromCallout(callout: Callout): string {
+	const matches = /^(.)(.*)/u.exec(callout.id);
+	if (matches == null) return callout.id;
 
-	constructor(previousQAVersion: string) {
-		super(app);
-		this.previousVersion = previousQAVersion;
+	const firstChar = matches[1].toLocaleUpperCase();
+	const remainingChars = matches[2].toLocaleLowerCase().replace(/-+/g, " ");
 
-		this.releaseNotesPromise = getReleaseNotesAfter(
-			"chhoumann",
-			"quickadd",
-			previousQAVersion
-		);
-		this.releaseNotesPromise
-			.then((releases) => {
-				this.releases = releases;
+	return `${firstChar}${remainingChars}`;
+}
 
-                if (this.releases.length === 0) {
-                    this.close();
-                    return;
-                }
-
-				this.display();
-			})
-			.catch((err) => {
-				console.log(`Failed to fetch release notes: ${err as string}`);
-			});
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.empty();
-		contentEl.createEl("h1", {
-			text: "Fetching release notes...",
-		});
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-
-	private display(): void {
-		const { contentEl } = this;
-		contentEl.empty();
-		contentEl.classList.add("quickadd-update-modal-container");
-
-        const header = `### New in QuickAdd v${this.releases[0].tag_name}\n`
-		const text = `Thank you for using QuickAdd! If you like the plugin, please consider supporting me by buying me a coffee. With your sponsorship, I'll be able to contribute more to my existing projects, start new ones, and be more responsive to issues & feature requests.`;
-		const buymeacoffee = `<div class="quickadd-bmac-container"><a href="https://www.buymeacoffee.com/chhoumann" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 40px !important;width: 144px !important;" ></a></div>`;
-
-		const contentDiv = contentEl.createDiv("quickadd-update-modal");
-		const releaseNotes = this.releases
-			.map((release) => release.body)
-			.join("\n---\n");
-
-		const andNow = `And now, here is everything new in QuickAdd since your last update (v${this.previousVersion}):`;
-        const feedbackForm = `I'd love to get your feedback on QuickAdd! Please fill out this <a href="https://forms.gle/WRq1ewcKK8qmkqps6">feedback form</a> to let me know what you think.`;
-		const markdownStr = `${header}\n${text}\n${buymeacoffee}\n${feedbackForm}\n\n${andNow}\n\n---\n\n${addExtraHashToHeadings(
-			releaseNotes
-		)}`;
-
-		void MarkdownRenderer.renderMarkdown(
-			markdownStr,
-			contentDiv,
-			app.vault.getRoot().path,
-			new Component(),
-		);
-	}
+/**
+ * Gets the color (as a {@link RGB}) from a {@link Callout}.
+ * This will try to do basic parsing on the color field.
+ *
+ * @param callout The callout.
+ * @returns The callout's color, or null if not valid.
+ */
+export function getColorFromCallout(callout: Callout): RGB | null {
+	return parseColorRGB(`rgb(${callout.color})`);
 }
