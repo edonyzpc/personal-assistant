75f684cff998bb7156f2a2b0914cdfb6b3ee2850
edonyzpc <edonyzpc@yahoo.com>
2025-12-07 17:41:05 +0800

    refactor: replace svelte record/stat views with react

--- src/components/AIIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { ThemeIcon } from "@svelteuidev/core";
-	import { ModulzLogo } from "svelte-radix";
-</script>
-
-<ThemeIcon size="xl" variant="gradient" gradient={{ from: "red", deg: 100 }}>
-	<ModulzLogo id="modulz-log-icon" color="rgb(134 239 172)" />
-</ThemeIcon>
